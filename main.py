84587ca76704c50cdddebd0069120406ab1a89fd
Naichen Zhao <nzhao11235@berkeley.edu>
2022-11-18 02:50:42 -0800

    added min time + optimized code

--- main.py
+++ main.py
@@ -8,11 +8,10 @@ from utils import *
 #  |           Constant values                 |
 #  +-------------------------------------------+
  
-X_GRID = 11
-Y_GRID = 1
+X_GRID = 51
+Y_GRID = 51
 Z_GRID = 1
 r = 1
-dt = 1
 
 
 #  +-------------------------------------------+
@@ -42,6 +41,7 @@ def main():
     #  +-------------------------------------------+
     #  |           Setup Matrix                    |
     #  +-------------------------------------------+
+    print("Setting up matrix... ")
 
     mat = np.empty((X_GRID, Y_GRID, Z_GRID, dp_length))
 
@@ -49,17 +49,46 @@ def main():
         for j in range(Y_GRID):
             for k in range(Z_GRID):
                 set_point(mat, (i, j, k), make_point())
+
+    min_time = get_min_timestep(mat)
     
-    next_points = set_mat(mat, (100, 100), ((0, 0, 0), (10, 0, 0)))
+    TEMPS = (100, 100, 100, 100)
+    POINTS = ((0, 0, 0), (50, 50, 0), (3, 0, 0), (25, 25, 0))
+
+    next_points = set_mat(mat, TEMPS, POINTS)
 
 
     #  +-------------------------------------------+
     #  |              Run Loop                     |
     #  +-------------------------------------------+
-    for i in tqdm(range(3)):
-        next_points = sim(mat, next_points, dt)
+    print("Running loop... ")
+    dt = 0.25
+    NUM_CYCLES = 400
 
-    print(get_mat_temp(mat))
+    if dt > min_time:
+        print("defined time is too large for stability")
+        print("new timestep of", min_time, "has been selected")
+        dt = min_time
+
+
+
+    for i in tqdm(range(NUM_CYCLES)):
+        next_points = sim(mat, next_points, dt)
+    
+
+
+    #  +-------------------------------------------+
+    #  |           Print Values                    |
+    #  +-------------------------------------------+
+    print("Printing... ")
+
+    xy_grid = np.transpose(get_z_temp(mat))
+
+    # print(xy_grid)
+
+    p = plt.imshow(xy_grid, extent=[0,X_GRID,0,Y_GRID], cmap='gist_heat', vmax = 120)
+    plt.colorbar(p)
+    plt.show()
 
 
 
@@ -71,28 +100,39 @@ def set_mat(mat, temps, coordinates):
         curr_temp = temps[i]
 
         set_point(mat, curr_loc, make_point(t=curr_temp, s = 1))
-
         r_set.add(curr_loc)
         r_set.update(get_adj(mat, curr_loc))
-    
     return r_set
 
 
+
 def sim(mat, vals, dt):
-    ret_set = set()
-    temp_mat = np.zeros((mat.shape[0], mat.shape[1], mat.shape[2]))
+    r_set = set()
+    new_temps = {}
 
     for p in vals:
         t, points = calculate_dt_l(mat, p, dt)
-        set_point(temp_mat, p, t)
-        ret_set.update(points)
+        new_temps[p] = t
+        r_set.update(points)
 
-    for p in vals:
-        set_t(get_point(mat, p), get_point(temp_mat, p))
+    for p in new_temps.keys():
+        set_t(get_point(mat, p), new_temps[p])
 
-    return ret_set
+    return r_set
 
 
+
+def get_min_timestep(mat):
+    min_t = 100000;
+    for i in range(X_GRID):
+        for j in range(Y_GRID):
+            for k in range(Z_GRID):
+                curr_p = get_point(mat, (i, j, k))
+                a = get_cp(curr_p)*get_p(curr_p)
+                b = 3*get_dx(curr_p)*get_dy(curr_p)*get_dz(curr_p)
+                curr_t = 0.9 * a/b
+                min_t = min(curr_t, min_t)
+    return min_t
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,5 +1,4 @@
 import numpy as np
-from itertools import product
 
 def get_point(mat, p):
     if not point_exists(mat, p):
@@ -13,7 +12,7 @@ def set_point(mat, p, new_p):
     mat[p[0]][p[1]][p[2]] = new_p
 
 
-def make_point(t=0, dx=0.5, dy=0.5, dz=0.5, k=1, cp=1, p=4, s=False):
+def make_point(t=0, dx=0.5, dy=0.5, dz=0.5, k=1, cp=1, p=1, s=False):
     return [t, dx, dy, dz, s, k, cp, p]
 
 
@@ -47,75 +46,73 @@ def get_p(p):
     return p[7]
 
 
-def calculate_dt(mat, p, dt, r):
+def calculate_dt(mat, p, dt, r=1):
     curr_point = get_point(mat, p)
     adj_points = get_adj(mat, p)
 
     curr_t = get_t(curr_point)
     cp = get_cp(curr_point)
-    p=get_p(curr_point)
+    p = get_p(curr_point)
 
     a = 1 - (dt/(p*cp))*(len(adj_points)/r**3)
     b = (dt/(p*cp))*(1/r**3)
     sum_t = sum([get_t(get_point(mat, i)) for i in adj_points])
     new_t = curr_t*a + b*sum_t
 
-    return new_t, adj_points
+    return new_t, set(adj_points)
 
 
 def calculate_dt_l(mat, p, dt):
     a_sum = 0
     b_sum = 0
+
+    x = p[0]
+    y = p[1]
+    z = p[2]
+
     adj_points = get_adj(mat, p)
     curr_p = get_point(mat, p)
     
     # ---------- Calculate X points ----------
-    print(p_add(p, (1, 0, 0)))
-    if point_exists(mat, p_add(p, (1, 0, 0))):  # Get point for (1, 0, 0)
-        target = get_point(mat, p_add(p, (1, 0, 0)))
-        A = get_dz(curr_p) * get_dy(curr_p) * 4
-        a_sum += (1/A) * (1/(get_dx(curr_p) + get_dx(target)))
-        b_sum += (1/A) * (get_t(target)/(get_dx(curr_p) + get_dx(target)))
-        print(get_dx(curr_p))
+    A = curr_p[3] * curr_p[2] # dz * dy
+
+    if x+1 < mat.shape[0]:  # Get point for (1, 0, 0)
+        target = get_point(mat, (x+1, y, z))
+        a_sum += 1/((curr_p[1] + target[1]) * A)
+        b_sum += target[0]/((curr_p[1] + target[1]) * A)
     
-    if point_exists(mat, p_add(p, (-1, 0, 0))):  # Get point for (-1, 0, 0)
-        target = get_point(mat, p_add(p, (-1, 0, 0)))
-        A = get_dz(curr_p) * get_dy(curr_p) * 4
-        a_sum += (1/A) * (1/(get_dx(curr_p) + get_dx(target)))
-        b_sum += (1/A) * (get_t(target)/(get_dx(curr_p) + get_dx(target)))
+    if x-1 >= 0:  # Get point for (-1, 0, 0)
+        target = get_point(mat, (x-1, y, z))
+        a_sum += 1/((curr_p[1] + target[1]) * A)
+        b_sum += target[0]/((curr_p[1] + target[1]) * A)
 
     # ---------- Calculate Y points ----------
-    if point_exists(mat, p_add(p, (0, 1, 0))):  # Get point for (0, 1, 0)
-        target = get_point(mat, p_add(p, (0, 1, 0)))
-        A = get_dz(curr_p) * get_dx(curr_p) * 4
-        a_sum += (1/A) * (1/(get_dy(curr_p) + get_dy(target)))
-        b_sum += (1/A) * (get_t(target)/(get_dy(curr_p) + get_dy(target)))
+    A = curr_p[3] * curr_p[1] # dz * dx
+    if y+1 < mat.shape[1]:  # Get point for (0, 1, 0)
+        target = get_point(mat, (x, y+1, z))
+        a_sum += 1/((curr_p[2] + target[2]) * A)
+        b_sum += target[0]/((curr_p[2] + target[2]) * A)
 
-    if point_exists(mat, p_add(p, (0, -1, 0))):  # Get point for (0, -1, 0)
-        target = get_point(mat, p_add(p, (0, -1, 0)))
-        A = get_dz(curr_p) * get_dx(curr_p) * 4
-        a_sum += (1/A) * (1/(get_dy(curr_p) + get_dy(target)))
-        b_sum += (1/A) * (get_t(target)/(get_dy(curr_p) + get_dy(target)))
+    if y-1 >= 0:  # Get point for (0, -1, 0)
+        target = get_point(mat, (x, y-1, z))
+        a_sum += 1/((curr_p[2] + target[2]) * A)
+        b_sum += target[0]/((curr_p[2] + target[2]) * A)
 
     # ---------- Calculate Z points ----------
-    if point_exists(mat, p_add(p, (0, 0, 1))):  # Get point for (0, 0, 1)
-        target = get_point(mat, p_add(p, (0, 0, 1)))
-        A = get_dx(curr_p) * get_dy(curr_p) * 4
-        a_sum += (1/A) * (1/(get_dz(curr_p) + get_dz(target)))
-        b_sum += (1/A) * (get_t(target)/(get_dz(curr_p) + get_dz(target)))
+    A = curr_p[1] * curr_p[2]  # dx * dy
+    if z+1 < mat.shape[2]:  # Get point for (0, 0, 1)
+        target = get_point(mat, (x, y, z+1))
+        a_sum += 1/((curr_p[3] + target[3]) * A)
+        b_sum += target[0]/((curr_p[3] + target[3]) * A)
     
-    if point_exists(mat, p_add(p, (0, 0, -1))):  # Get point for (0, 0, -1)
-        target = get_point(mat, p_add(p, (0, 0, -1)))
-        A = get_dx(curr_p) * get_dy(curr_p) * 4
-        a_sum += (1/A) * (1/(get_dz(curr_p) + get_dz(target)))
-        b_sum += (1/A) * (get_t(target)/(get_dz(curr_p) + get_dz(target)))
+    if z-1 >= 0:  # Get point for (0, 0, -1)
+        target = get_point(mat, (x, y, z-1))
+        a_sum += 1/((curr_p[3] + target[3]))/A
+        b_sum += target[0]/((curr_p[3] + target[3]))/A
 
-    a = get_t(curr_p) * (1 - (dt/(get_cp(curr_p) * get_p(curr_p))) * a_sum)
-    b = (dt/(get_cp(curr_p) * get_p(curr_p))) * b_sum
-
-    new_t =  a + b
-
-    return new_t, adj_points
+    a = curr_p[0] * (1 - (dt/(get_cp(curr_p) * get_p(curr_p))) * a_sum/4)
+    b = (dt/(get_cp(curr_p) * get_p(curr_p))) * b_sum/4
+    return a + b, adj_points
 
     
 
@@ -124,15 +121,31 @@ def get_mat_temp(mat):
     return mat[:,:,:,0]
 
 
-def get_adj(mat, p):
-    ret = []
-    mask = [(1,0,0), (0,1,0), (0,0,1)]
+def get_z_temp(mat, z=0):
+    return mat[:, :, z, 0]
 
-    for i in mask:
-        if point_exists(mat, p_add(p, i)):
-            ret.append(p_add(p, i))
-        if point_exists(mat, p_sub(p, i)):
-            ret.append(p_sub(p, i))
+
+def get_adj(mat, p):
+    ret = set()
+
+    x = p[0]
+    y = p[1]
+    z = p[2]
+
+    if (x + 1 < mat.shape[0]):
+        ret.add((x+1, y, z))
+    if (y + 1 < mat.shape[1]):
+        ret.add((x, y+1, z))
+    if (z + 1 < mat.shape[2]):
+        ret.add((x, y, z+1))
+    
+    if (x - 1 >= 0):
+        ret.add((x-1, y, z))
+    if (y - 1 >= 0):
+        ret.add((x, y-1, z))
+    if (z - 1 >= 0):
+        ret.add((x, y, z-1))
+
     return ret
 
 
