85c50d84cde1d0297aa9f61a8e12f2e43a142ea6
Naichen Zhao <nzhao11235@berkeley.edu>
2022-12-19 18:28:14 -0800

    cut down on code

--- utils.py
+++ utils.py
@@ -38,123 +38,12 @@ def get_p(p):
 
 
 #  +------------------------------------------------+
-#  |             Calculation Utils                  |
-#  +------------------------------------------------+
-
-def calculate_dt(mat_d, mat_t, p, dt, r=1):
-    curr_point = get_point(mat_d, p)
-    adj_points = get_adj(mat_d, p)
-
-    curr_t = get_point(mat_t, p)
-    cp = get_cp(curr_point)
-    p = get_p(curr_point)
-
-    a = 1 - (dt/(p*cp))*(len(adj_points)/r**3)
-    b = (dt/(p*cp))*(1/r**3)
-    sum_t = sum([get_point(mat_t, i) for i in adj_points])
-    new_t = curr_t*a + b*sum_t
-
-    return new_t, set(adj_points)
-
-
-def calculate_dt_l(mat_d, mat_t, p, dt):
-    a_sum = 0
-    b_sum = 0
-
-    x = p[0]
-    y = p[1]
-    z = p[2]
-
-    adj_points = get_adj(mat_d, p)
-    curr_p = get_point(mat_d, p)
-    curr_t = get_point(mat_t, p)
-    
-    # ---------- Calculate X points ----------
-    A = curr_p[2] * curr_p[1] # dz * dy
-
-    if x+1 < mat_d.shape[0]:  # Get point for (1, 0, 0)
-        target_p = get_point(mat_d, (x+1, y, z), check=False)
-        target_t = get_point(mat_t, (x+1, y, z), check=False)
-
-        a_sum += 1/((curr_p[0] + target_p[0]) * A)
-        b_sum += target_t/((curr_p[0] + target_p[0]) * A)
-    
-    if x-1 >= 0:  # Get point for (-1, 0, 0)
-        target_p = get_point(mat_d, (x-1, y, z), check=False)
-        target_t = get_point(mat_t, (x-1, y, z), check=False)
-
-        a_sum += 1/((curr_p[0] + target_p[0]) * A)
-        b_sum += target_t/((curr_p[0] + target_p[0]) * A)
-
-    # ---------- Calculate Y points ----------
-    A = curr_p[2] * curr_p[0] # dz * dx
-    if y+1 < mat_d.shape[1]:  # Get point for (0, 1, 0)
-        target_p = get_point(mat_d, (x, y+1, z), check=False)
-        target_t = get_point(mat_t, (x, y+1, z), check=False)
-
-        a_sum += 1/((curr_p[1] + target_p[1]) * A)
-        b_sum += target_t/((curr_p[1] + target_p[1]) * A)
-
-    if y-1 >= 0:  # Get point for (0, -1, 0)
-        target_p = get_point(mat_d, (x, y-1, z), check=False)
-        target_t = get_point(mat_t, (x, y-1, z), check=False)
-
-        a_sum += 1/((curr_p[1] + target_p[1]) * A)
-        b_sum += target_t/((curr_p[1] + target_p[1]) * A)
-
-    # ---------- Calculate Z points ----------
-    A = curr_p[0] * curr_p[1]  # dx * dy
-    if z+1 < mat_d.shape[2]:  # Get point for (0, 0, 1)
-        target_p = get_point(mat_d, (x, y, z+1), check=False)
-        target_t = get_point(mat_t, (x, y, z+1), check=False)
-
-        a_sum += 1/((curr_p[2] + target_p[2]) * A)
-        b_sum += target_t/((curr_p[2] + target_p[2]) * A)
-    
-    if z-1 >= 0:  # Get point for (0, 0, -1)
-        target_p = get_point(mat_d, (x, y, z-1), check=False)
-        target_t = get_point(mat_t, (x, y, z-1), check=False)
-
-        a_sum += 1/((curr_p[2] + target_p[2]))/A
-        b_sum += target_t/((curr_p[2] + target_p[2]))/A
-
-    a = curr_t * (1 - (dt/(get_cp(curr_p) * get_p(curr_p))) * a_sum/4)
-    b = (dt/(get_cp(curr_p) * get_p(curr_p))) * b_sum/4
-    return a + b, adj_points
-
-
-#  +------------------------------------------------+
 #  |             Helper Functions                   |
 #  +------------------------------------------------+
 
 
 def get_z_temp(mat_t, z=0):
     return mat_t[:, :, z]
-
-
-def get_adj(mat_d, p):
-    ret = set()
-
-    x = p[0]
-    y = p[1]
-    z = p[2]
-
-    if (x + 1 < mat_d.shape[0]):
-        ret.add((x+1, y, z))
-    if (y + 1 < mat_d.shape[1]):
-        ret.add((x, y+1, z))
-    if (z + 1 < mat_d.shape[2]):
-        ret.add((x, y, z+1))
-    
-    if (x - 1 >= 0):
-        ret.add((x-1, y, z))
-    if (y - 1 >= 0):
-        ret.add((x, y-1, z))
-    if (z - 1 >= 0):
-        ret.add((x, y, z-1))
-
-    return ret
-
 
 def point_exists(mat_d, p):
     x = p[0]
@@ -168,9 +57,3 @@ def point_exists(mat_d, p):
     return x_check and y_check and z_check
 
 
-def p_add(p1, p2):
-    return (p1[0] + p2[0], p1[1] + p2[1], p1[2] + p2[2])
-
-
-def p_sub(p1, p2):
-    return (p1[0] - p2[0], p1[1] - p2[1], p1[2] - p2[2])
